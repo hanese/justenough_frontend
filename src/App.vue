6199e7cd7a33285daa8b96a65bb9f785bd7d3dd8
Hannes Schöller <inf21132@lehre.dhbw-stuttgart.de>
2023-04-27 23:19:01 +0200

    checked if bearer authorization works, it did

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <script setup>
 import HeaderWithNavbar from "@/components/HeaderWithNavbar.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
+import GetUserData from "@/components/GetUserData.vue";
+import LoginForm from "@/components/LoginForm.vue";
 </script>
 
 <template>
 
-  <header>
-    <HeaderWithNavbar/>
-    <router-view/>
-    <HelloWorld/>
-  </header>
+    <header>
+        <HeaderWithNavbar/>
+        <router-view/>
+        <HelloWorld/>
+        <LoginForm/>
+        <GetUserData/>
+    </header>
 
-  <main>
+    <main>
 
-  </main>
+    </main>
 
 </template>
 
 <style scoped>
 header {
-  line-height: 4;
+    line-height: 4;
 }
 </style>
