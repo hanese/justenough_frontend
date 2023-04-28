b1a25b4b8d0a453cace3b02cade103695c57ecef
Eisebrain <inf21208@lehre.dhbw-stuttgart.de>
2023-04-28 10:08:15 +0200

    http angepasst

--- src/components/GetUserData.vue
+++ src/components/GetUserData.vue
@@ -23,7 +23,7 @@ export default {
                 return;
             }
 
-            const response = await fetch('https://localhost:8000/users/me/', {
+            const response = await fetch('http://localhost:8000/users/me/', {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
