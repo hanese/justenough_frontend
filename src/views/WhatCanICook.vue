898e5b65ce79a7d9ca5a5c15efa87ae2d1c223a2
hanese <hannes.schoeller@t-online.de>
2023-05-13 10:10:57 +0200

    wcic ist fertig gemacht

--- src/views/WhatCanICook.vue
+++ src/views/WhatCanICook.vue
@@ -1,40 +1,42 @@
 <script setup>
 function getTokenFromCookie() {
-  const cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith('access_token=')) {
-      return cookie.substring('access_token='.length, cookie.length);
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith('access_token=')) {
+            return cookie.substring('access_token='.length, cookie.length);
+        }
     }
-  }
-  return null;
+    return null;
 }
 
-async function getRecipesByStorageIngredients(){
-
-  const token = getTokenFromCookie()
-  const options = {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
-  };
-  const response = await fetch('http://localhost:8000/api/storage/getStorage', options)
-  const data = await response.json()
-
-  const url = new URL('https://example.com/api/recipes');
-  ingredients.forEach(ingredient => {
-    url.searchParams.append('ingredient', ingredient);
-  });
-  const recipes = await fetch('http://api/recipes/getRecipesByIngredients', options)
+async function getRecipesByStorageIngredients() {
+    const token = getTokenFromCookie()
+    const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    };
+    const storageResponse = await fetch('http://localhost:8000/api/storage/getStorage', options)
+    const storage = await storageResponse.json()
+    const ingredients = storage.map(ingredient => ingredient.item)
+    const url = new URL('http://localhost:8000/api/recipes/getRecipesByIngredients');
+    ingredients.forEach(ingredient => {
+        url.searchParams.append('ingredients', ingredient);
+    });
+    const recipesResponse = await fetch(url, options)
+    const recipes = await recipesResponse.json()
 
 }
+
+getRecipesByStorageIngredients()
 </script>
 
 
 <template>
-  <h1>You can cook the following recipes:</h1>
+    <h1>You can cook the following recipes:</h1>
 
 </template>
 
