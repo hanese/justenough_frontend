47ee0e24233cf3cb707fb39f1d73c9f3d75cc8c6
Eisebrain <inf21208@lehre.dhbw-stuttgart.de>
2023-05-16 22:20:24 +0200

    Rezept erstellen Frontend Logik müsste passen

--- src/views/CreateRecipe.vue
+++ src/views/CreateRecipe.vue
@@ -15,8 +15,13 @@
       <input type="text" class="form-control" v-model="ingredient.measure" :placeholder="'Measure ' + (index + 1)">
     </div>
 
-    <button @click="addIngredientField" class="btn btn-primary">Add Ingredient</button>
+    <div class="input-group mb-3">
+      <button @click="addIngredientField" class="btn btn-secondary"><span class="material-symbols-outlined" style="padding-top: 6px">add</span></button>
+    </div>
+
     <p v-if="ingredients.length === 0" class="text-danger">Please provide at least one ingredient.</p>
+
+    <button @click="saveRecipe" class="btn btn-success">Save Recipe</button>
   </div>
 </template>
 
@@ -36,6 +41,60 @@ export default {
         this.ingredients.push({ name: '', measure: '' });
       }
     },
+    saveRecipe() {
+      const recipeData = {
+        meal: this.mealName || 'Null',
+        category: 'Null',
+        area: 'Null',
+        instructions: this.mealInstructions || 'Null',
+        meal_thumb: 'Null',
+        tags: 'Null',
+        youtube: 'Null',
+        ...this.getIngredientsData(),
+        source: 'Null',
+        image_source: 'Null',
+        creative_commons_confirmed: 'Null',
+        date_modified: 'Null',
+      };
+
+      // Send the recipe data to the API endpoint
+      this.postRecipeData(recipeData);
+    },
+    getIngredientsData() {
+      const ingredientsData = {};
+      for (let i = 0; i < this.ingredients.length; i++) {
+        const ingredientKey = `ingredient${i + 1}`;
+        const measureKey = `measure${i + 1}`;
+        const ingredientValue = this.ingredients[i].name || 'Null';
+        const measureValue = this.ingredients[i].measure || 'Null';
+        ingredientsData[ingredientKey] = ingredientValue;
+        ingredientsData[measureKey] = measureValue;
+      }
+      return ingredientsData;
+    },
+    async postRecipeData(recipeData) {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(recipeData),
+      };
+      try {
+        const response = await fetch('/api/recipes/postCustomRecipe', options);
+        if (response.ok) {
+          console.log('hat geklappt');
+          await this.$router.push('/Rezepte');
+
+        } else {
+          console.error('och nööööö');
+
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    },
   },
 };
 </script>
+
