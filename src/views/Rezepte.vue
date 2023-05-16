5b4d7f924d9b3cc3e4101099e87f4a32d87c88bb
Eisebrain <inf21208@lehre.dhbw-stuttgart.de>
2023-05-16 22:27:29 +0200

    Rezepte und WCIC Navigation

--- src/views/Rezepte.vue
+++ src/views/Rezepte.vue
@@ -7,14 +7,8 @@ import SearchbarTest from "@/components/deprecated_SearchbarTest.vue";
 <template>
   <div class="ZutatenHeader">
 
-    <div>
-      <h1 class="WillkommenH2">Recipes</h1>
-    </div>
-
-    <div>
-
-      <router-link to="WhatCanICook"><h2>What can I cook?</h2></router-link>
-    </div>
+    <router-link to="WhatCanICook"><h1 class="nichtaktuelleSeite">What Can I Cook?</h1></router-link>
+    <h1 class="aktuelleSeite">Recipes</h1>
 
     <div class="SearchFilterAdd">
       <SearchFilterAddRecipes/>
@@ -31,18 +25,68 @@ import SearchbarTest from "@/components/deprecated_SearchbarTest.vue";
 </template>
 
 <style scoped>
-h1{
-  text-align: center;
-  padding-bottom: 20px;
-  font-size: 30pt;
+li {
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  margin-bottom: 10px;
 }
 
-.WillkommenH2 {
+li:hover::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(169, 114, 114, 0.15);
+  z-index: -1;
+}
+
+.ingredient-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.ingredient-name {
+  margin-right: 10px;
+}
+
+.hidden-button {
+  display: none;
+}
+
+li:hover .hidden-button {
+  display: inline-block;
+}
+
+.aktuelleSeite {
   transition: all 0.2s ease-in-out;
   margin-top: 50px;
+  color: black;
+  background-color: rgba(169, 114, 114, 0.34); /* Hintergrundfarbe */
+  display: inline-block; /* Um den Rahmen um den Text zu setzen */
+  padding: 5px; /* Abstand innerhalb des Rahmens */
+  margin-right: 10px; /* Abstand zwischen "Ingredients" und "Storage" */
+  border: none;
+  border-radius: 20px;
 }
 
-.WillkommenH2:hover{
+.aktuelleSeite:hover{
+  color: rgba(169, 114, 114, 0.98);
+  transform: scale(1.1);
+  cursor: pointer;
+}
+
+.nichtaktuelleSeite{
+  transition: all 0.2s ease-in-out;
+  margin-top: 50px;
+  color: black;
+}
+
+.nichtaktuelleSeite:hover{
   color: #a97272;
   transform: scale(1.1);
   cursor: pointer;
--- src/views/WhatCanICook.vue
+++ src/views/WhatCanICook.vue
@@ -52,6 +52,11 @@ onMounted(async () => {
 
 
 <template>
+  <div>
+    <router-link to="Rezepte"><h1 class="nichtaktuelleSeite">Recipes</h1></router-link>
+    <h1 class="aktuelleSeite">What Can I Cook?</h1>
+
+  </div>
   <h1 v-if="isLoading">Loading...</h1>
   <h1 v-else>You can cook the following recipes:</h1>
   <ul v-if="!isLoading">
@@ -68,5 +73,70 @@ onMounted(async () => {
 </template>
 
 <style scoped>
+li {
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  margin-bottom: 10px;
+}
 
+li:hover::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(169, 114, 114, 0.15);
+  z-index: -1;
+}
+
+.ingredient-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.ingredient-name {
+  margin-right: 10px;
+}
+
+.hidden-button {
+  display: none;
+}
+
+li:hover .hidden-button {
+  display: inline-block;
+}
+
+.aktuelleSeite {
+  transition: all 0.2s ease-in-out;
+  margin-top: 50px;
+  color: black;
+  background-color: rgba(169, 114, 114, 0.34); /* Hintergrundfarbe */
+  display: inline-block; /* Um den Rahmen um den Text zu setzen */
+  padding: 5px; /* Abstand innerhalb des Rahmens */
+  margin-right: 10px; /* Abstand zwischen "Ingredients" und "Storage" */
+  border: none;
+  border-radius: 20px;
+}
+
+.aktuelleSeite:hover{
+  color: rgba(169, 114, 114, 0.98);
+  transform: scale(1.1);
+  cursor: pointer;
+}
+
+.nichtaktuelleSeite{
+  transition: all 0.2s ease-in-out;
+  margin-top: 50px;
+  color: black;
+}
+
+.nichtaktuelleSeite:hover{
+  color: #a97272;
+  transform: scale(1.1);
+  cursor: pointer;
+}
 </style>
